eb449dc6a23603471714953850b92868a2f43669
Nate <minecraftcrusher100@gmail.com>
2022-08-14 23:55:14 +0000

    Youtube scraper

---
+++ caspian/scrapers/youtube.py
@@ -0,0 +1,20 @@
+"""Module for scraping YouTube data."""
+import asyncio
+
+from caspian.scrapers.base import Scraper
+
+
+class YoutubeScraper(Scraper):
+    """Scraper for Youtube."""
+
+    async def start(
+        self, stdout, stderr, stdin
+    ) -> asyncio.subprocess.Process:  # pylint: disable=no-member
+        """Starts the YouTube scraper subprocess."""
+        return await asyncio.create_subprocess_shell(
+            f'youtube-dl --add-metadata --yes-playlist -x --audio-format mp3 --audio-quality 0 --retries infinite --socket-timeout 30 --prefer-ffmpeg --no-call-home -i --output "{self.download_dir}/%(channel)s - %(title)s.%(ext)s" "{self.url}"',
+            stdin=stdin,
+            stdout=stdout,
+            stderr=stderr,
+            close_fds=True,
+        )
