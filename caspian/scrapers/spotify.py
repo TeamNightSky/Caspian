cb5168e8ca7037a87e882bc7ba6821551f1faac1
Nate <minecraftcrusher100@gmail.com>
2022-08-14 23:04:00 +0000

    Spotify scraper

---
+++ caspian/scrapers/spotify.py
@@ -0,0 +1,21 @@
+"""Module for scraping Spotify data."""
+
+import asyncio
+
+from caspian.scrapers.base import Scraper
+
+
+class SpotifyScraper(Scraper):
+    """Scraper for Spotify."""
+
+    async def start(
+        self, stdout, stderr, stdin
+    ) -> asyncio.subprocess.Process:  # pylint: disable=no-member
+        """Start the scraper subprocess"""
+        return await asyncio.create_subprocess_shell(
+            f'spotdl "{self.url}" --path-template "{self.download_dir}/{{artist}} - {{title}}.{{ext}}"',
+            stdin=stdin,
+            stdout=stdout,
+            stderr=stderr,
+            close_fds=True,
+        )
