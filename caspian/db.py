1f22a898831aa5d4a3aace354f710b71c7286d8d
GrandMoff100 <minecraftcrusher100@gmail.com>
2022-08-16 18:04:37 -0500

    Fix supabase url env var usage

--- caspian/db.py
+++ caspian/db.py
@@ -2,10 +2,8 @@ import os
 from supabase import create_client, Client
 
 
-print(os.environ)
-
 class DB:
     client: Client = create_client(
-        os.environ["SUPABASE_URL"],
+        "0.0.0.0",
         os.environ["SUPABASE_KEY"],
     )
