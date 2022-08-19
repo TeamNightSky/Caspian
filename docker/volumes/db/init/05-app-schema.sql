2abbac4a5132ff836ef1055973082d7b162c1ee1
GrandMoff100 <minecraftcrusher100@gmail.com>
2022-08-19 14:11:40 -0500

    Add foreign key contraints

--- docker/volumes/db/init/05-app-schema.sql
+++ docker/volumes/db/init/05-app-schema.sql
@@ -1,7 +1,10 @@
-create schema if not exists app_schema authentication authenticated;
+create schema if not exists app_schema authentication supabase_admin;
 
+grant usage on schema storage to postgres, authenticated, service_role;
+
+/* Table of all songs (shared between users) */
 create app_schema.song_metadata (
-    "song_path" text primary key,
+    "song_path" text UNIQUE,
     "uuid" uuid default uuid_generate_v4(),
     "title" text,
     "artist" text,
@@ -13,11 +16,14 @@ create app_schema.song_metadata (
     "plays" integer,
     "cover_path" text default null,
     "source" text,
-    "created_at" timestamp default current_timestamp,
-    "updated_at" timestamp default current_timestamp
+    "created_at" timestamptz default current_timestamp,
+    "updated_at" timestamptz default current_timestamp,
+    primary key ("uuid")
 );
 
+/* Table of scraper logs */
 CREATE app_schema.scraper_logs (
+    "uuid" uuid default uuid_generate_v4() primary key,
     "source" text,
     "url" text,
     "log_path" text,
@@ -25,23 +31,27 @@ CREATE app_schema.scraper_logs (
     "status" integer,
 );
 
+/* Table of user-specific song-specific statistics */
 CREATE app_schema.song_stats (
     "user_id" uuid UNIQUE,
-    "song_path" text UNIQUE,
+    "song_id" uuid UNIQUE,
     "plays" integer default 0,
     "liked" boolean default false,
     "disliked" boolean default false,
-    primary key ("user_id", "song_file")
+    primary key ("user_id", "song_id")
+    CONSTRAINT "user_playlists_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "auth"."users"("id")
+    CONSTRAINT "user_playlists_song_id_fkey" FOREIGN KEY ("song_id") REFERENCES "app_schema"."song_metadata"("uuid")
 );
 
-
+/* Table of user playlists */
 CREATE app_schema.user_playlists (
     "user_id" uuid UNIQUE,
     "playlist_id" uuid UNIQUE default uuid_generate_v4(),
     "name" text,
     "cover_path" text default null,
     "songs" text[],
-    "created_at" timestamp default current_timestamp,
-    "updated_at" timestamp default current_timestamp,
-    primary key ("user_id", "playlist_id")
+    "created_at" timestamptz default current_timestamp,
+    "updated_at" timestamptz default current_timestamp,
+    primary key ("playlist_id")
+    CONSTRAINT "user_playlists_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "auth"."users"("id")
 );
