1695ebb01902817d819f4fd818796dd3a0fe1ea0
Nate <nlarsen23.student@gmail.com>
2023-06-18 18:26:37 -0500

    Clean codebase

--- api/app.py
+++ api/app.py
@@ -1,10 +1,6 @@
-import os
-import asyncio
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from api.db import DB
 from .routes import scrapers_router, db_router
 
 
@@ -17,24 +13,5 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-app.include_router(scrapers_router, prefix="/api")
-app.include_router(db_router, prefix="/api")
-
-
-@app.on_event("startup")
-async def startup():
-    """
-    Initialize the storage buckets
-    """
-    while True:
-        try:
-            for bucket in await DB.storage.list_buckets():
-                if bucket.name == "files":
-                    print("Bucket found.")
-                    return
-            await DB.storage.create_bucket("files")
-            print("Created bucket.")
-            break
-        except:  # If you are gonna complain, fix it yourself
-            print("Storage not launched. Retrying...")
-            await asyncio.sleep(1)
+app.mount("/api", scrapers_router)
+app.mount("/api", db_router)
