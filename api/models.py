7e5edad230e9a50401e39a757e4301a460e27795
Nate <nlarsen23.student@gmail.com>
2023-06-18 20:15:17 -0500

    Update models

--- api/models.py
+++ api/models.py
@@ -1,10 +1,25 @@
+import os
 from datetime import datetime
 import enum
-from sqlalchemy import Column, DateTime, Enum, Integer, ForeignKey, Text, LargeBinary
-from sqlalchemy.orm import declarative_base, Mapped, relationship
+from sqlalchemy import UUID, Column, DateTime, Enum, Integer, ForeignKey, Text, LargeBinary, create_engine
+from sqlalchemy.orm import declarative_base, Mapped, relationship, sessionmaker
 
+from api import DEBUG
 
 Base = declarative_base()
+
+POSTGRES_USER = os.environ["POSTGRES_USER"]
+POSTGRES_PASSWORD = os.environ["POSTGRES_PASSWORD"]
+POSTGRES_HOST = os.environ["POSTGRES_HOST"]
+POSTGRES_PORT = os.environ["POSTGRES_PORT"]
+POSTGRES_DB = os.environ["POSTGRES_DB"]
+
+ENGINE = create_engine(
+    f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}",
+    echo=DEBUG
+)
+
+Session = sessionmaker(bind=ENGINE)
 
 
 class Artist(Base):
@@ -17,10 +32,14 @@ class Artist(Base):
 
 
 class Playlist(Base):
+    __tablename__ = "playlists"
+
     id = Column(Integer, primary_key=True)
     name = Column(Text, nullable=False, unique=True)
 
-    songs
+    owner_id = Column(UUID(as_uuid=True), ForeignKey("auth.user.id"), nullable=False)
+    songs: Mapped[list["Song"]] = relationship("Song", secondary="playlist_songs")
+
 
 
 class Play(Base):
@@ -30,6 +49,7 @@ class Play(Base):
     song_id = Column(Integer, ForeignKey("songs.id"), nullable=False)
     timestamp = Column(DateTime, nullable=False)
 
+    user_id = Column(UUID(as_uuid=True), ForeignKey("auth.user.id"), nullable=False)
     song: Mapped["Song"] = relationship("Song", back_populates="plays")
 
 
@@ -48,6 +68,7 @@ class Song(Base):
     content_bytes = Column(LargeBinary, nullable=False)
     cover_bytes = Column(LargeBinary)
 
+    uploaded_by = Column(UUID(as_uuid=True), ForeignKey("auth.user.id"), nullable=False)
     artist: Mapped[Artist] = relationship("Artist", back_populates="songs")
 
 
@@ -65,6 +86,7 @@ class Scraper(Base):
     job_type = Column(Enum(JobType), nullable=False)
     url = Column(Text, nullable=False)
 
+    started_by = Column(UUID(as_uuid=True), ForeignKey("auth.user.id"), nullable=False)
     started = Column(DateTime, default=datetime.utcnow)
 
     def __repr__(self):
