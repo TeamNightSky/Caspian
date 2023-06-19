cf40ecbd221469c774a982f06078725f35e97e60
Nate <nlarsen23.student@gmail.com>
2023-06-18 23:11:49 -0500

    Initialize models on startup

--- api/app.py
+++ api/app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from .routes import scrapers_router, db_router
+from .models import Session, Base
 
 from api import DEBUG
 
@@ -17,3 +18,11 @@ app.add_middleware(
 )
 app.mount("/api", scrapers_router)
 app.mount("/api", db_router)
+
+
+@app.on_event("startup")
+async def initialize_models():
+    with Session() as db:
+        if DEBUG:
+            Base.metadata.drop_all(db.bind)
+        Base.metadata.create_all(db.bind)
