fe23e5e3bfa2111179c2096b6a5b9275d0db9b20
Nate <nlarsen23.student@gmail.com>
2023-06-20 22:56:25 -0500

    Add volume for worker and use consistent naming

--- api/__main__.py
+++ api/__main__.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     uvicorn.run(
         "api.app:app",
         host="0.0.0.0",
-        port=int(os.environ["PORT"]),
+        port=int(os.environ["CASPIAN_PORT"]),
         reload=DEBUG,
     )
