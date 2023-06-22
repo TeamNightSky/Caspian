54d9cc25640c7c5085c15dc627fb5ca5bac710e3
FoxNerdSaysMoo <zceboys@gmail.com>
2023-06-22 16:36:29 -0400

    Theoretically add auth to scrapers

--- api/routes/scrapers.py
+++ api/routes/scrapers.py
@@ -2,34 +2,48 @@ from fastapi import APIRouter, BackgroundTasks, FastAPI, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 
+import jwt
+
 from api.worker import scrape_spotify, scrape_youtube, upload_download
 
 router = APIRouter(prefix="/import")
 
+PUBLIC = bool(os.environ.get("CASPIAN_PUBLIC", "false").strip().lower() == "true")
+
+def verify(auth: str):
+    if PUBLIC:
+        return True, 'anon'
+    try:
+        payload = jwt.decode(auth, os.environ['JWT_SECRET'], algorithms=['HS256'])
+        if role == 'service_role':
+            return True, 'superuser'
+        return True, payload['sub']
+    except jwt.exceptions.InvalidTokenError:
+        return False, None
 
 @router.post("/youtube")
-async def youtube_scraper(url: str):
+async def youtube_scraper(url: str, authorization: str = None):
     """
     Scrape a song from Youtube
     """
-    # TODO: Add user_id to scrape_youtube
-    scrape_youtube.delay(url, None)
+    if (_, user_id := verify(authorization)):
+        scrape_youtube.delay(url, user_id)
 
 
 @router.post("/spotify")
-async def spotify_scraper(url: str):
+async def spotify_scraper(url: str, authorization: str = None):
     """
     Scrape a song from Spotify
     """
-    # TODO: Add user_id to scrape_spotify
-    scrape_spotify.delay(url, None)
+    if (_, user_id := verify(authorization)):
+        scrape_spotify.delay(url, user_id)
 
 
 @router.post("/upload")
-async def upload_file(file: UploadFile):
+async def upload_file(file: UploadFile, authorization: str = None):
     """
     Manually upload song
     """
-    # TODO: Add user_id to upload_download
-    upload_download.delay(file.file.read(), None)
+    if (_, user_id := verify(authorization)):
+        upload_download.delay(file.file.read(), user_id)
 
